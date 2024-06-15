41cfe0f15c5261595488dc841923917a03d57e0f
Gabriel Trabasso <gabr.j.t@gmail.com>
2024-06-15 18:08:32 -0300

    Move logic to system

--- main.cpp
+++ main.cpp
@@ -1,20 +1,34 @@
 #include <cstring>
 #include "cppengine.h"
 
-int main() {
-    Engine::Application &application = Engine::Application::Get();
-    raylib::Window &window = application.OpenWindow("CPP ENGINE", 1920, 1080);
-    const char *text = "WELCOME TO CPP ENGINE";
-    int length = strlen(text);
+class MenuSystem : public Engine::System {
+public:
+    void Update() override {
+        static Engine::Application &application = Engine::Application::Get();
+        static const char *text = "WELCOME TO CPP ENGINE";
+        static const int length = strlen(text);
+        std::shared_ptr<raylib::Window> window = application.GetWindow();
 
-    while (!window.ShouldClose()) {
         BeginDrawing();
 
-        window.ClearBackground(BLACK);
+        window->ClearBackground(BLACK);
 
-        DrawText(text, (window.GetWidth() / 2) - (length * 60 / 3), window.GetHeight() / 2, 60, RAYWHITE);
+        DrawText(text, (window->GetWidth() / 2) - (length * 60 / 3), window->GetHeight() / 2, 60, RAYWHITE);
 
         EndDrawing();
+    }
+};
+
+int main() {
+    Engine::Application &application = Engine::Application::Get();
+
+    std::array<std::unique_ptr<Engine::System>, 1> systems{std::make_unique<MenuSystem>()};
+    application.RegisterSystems(systems);
+
+    std::shared_ptr<raylib::Window> window = application.OpenWindow("CPP ENGINE", 1920, 1080);
+
+    while (!window->ShouldClose()) {
+        application.UpdateSystems();
     }
 
     return 0;
