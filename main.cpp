4b26bcc6d3163a556f5176a1b0a53b47e25efbd1
Gabriel Trabasso <gabr.j.t@gmail.com>
2024-06-15 21:36:08 -0300

    Add player and input systems

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <cstring>
+#include <iostream>
 #include "cppengine.h"
 
 class WindowSystem final : public Engine::System {
@@ -61,9 +62,72 @@ public:
     }
 };
 
+struct Input {
+    raylib::Vector2 Joystick;
+};
+
+struct Player {
+    static entt::entity Create(entt::registry &registry) {
+        const entt::entity entity = registry.create();
+
+        registry.emplace<Player>(entity);
+        registry.emplace<Input>(entity);
+
+        return entity;
+    }
+};
+
+class CreatePlayerSystem : public Engine::System {
+public:
+    CreatePlayerSystem() {
+        Player::Create(Engine::Application::Get().GetRegistry());
+    }
+
+    void Update() override {}
+};
+
+class PlayerInputSystem : public Engine::System {
+public:
+    void Update() override {
+        static entt::registry &registry = Engine::Application::Get().GetRegistry();
+        auto view = registry.view<const Player, Input>();
+
+        view.each([](Input &input) {
+            float x = 0, y = 0;
+
+            if (IsKeyDown(KEY_A)) {
+                x = -1;
+            } else if (IsKeyDown(KEY_D)) {
+                x = 1;
+            }
+
+            if (IsKeyDown(KEY_W)) {
+                y = -1;
+            } else if (IsKeyDown(KEY_S)) {
+                y = 1;
+            }
+
+            input.Joystick = raylib::Vector2(x, y);
+        });
+    }
+};
+
+class LogPlayerInputSystem : public Engine::System {
+public:
+    void Update() override {
+        static entt::registry &registry = Engine::Application::Get().GetRegistry();
+        auto view = registry.view<const Player, const Input>();
+
+        view.each([](const Input &input) {
+            std::cout << "Player Input: " << input.Joystick.ToString() << std::endl;
+        });
+    }
+};
+
 int main() {
     Engine::Application &application = Engine::Application::Get();
-    application.RegisterSystems(WindowSystem(), SpawnCircleSystem(), DrawCircleSystem());
+    application.RegisterSystems(WindowSystem(), CreatePlayerSystem(), PlayerInputSystem(), LogPlayerInputSystem(),
+                                SpawnCircleSystem(), DrawCircleSystem());
 
     std::shared_ptr<raylib::Window> window = application.GetWindow();
 
