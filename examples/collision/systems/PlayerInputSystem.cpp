c686cd3304cc39eb5eb3752a258288af1bca807f
Gabriel Trabasso <gabr.j.t@gmail.com>
2024-06-17 13:45:41 -0300

    Add support to arrow keys

--- examples/collision/systems/PlayerInputSystem.cpp
+++ examples/collision/systems/PlayerInputSystem.cpp
@@ -17,19 +17,19 @@ namespace Engine::Examples::Collision {
         view.each([](Input &input) {
             float x = 0, y = 0;
 
-            if (IsKeyDown(KEY_A)) {
+            if (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) {
                 x -= 1;
             }
 
-            if (IsKeyDown(KEY_D)) {
+            if (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT)) {
                 x += 1;
             }
 
-            if (IsKeyDown(KEY_W)) {
+            if (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP)) {
                 y -= 1;
             }
 
-            if (IsKeyDown(KEY_S)) {
+            if (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN)) {
                 y += 1;
             }
 
