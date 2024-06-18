1af45773e03838ddd3fd310d138c20f1818a3af3
Gabriel Trabasso <gabr.j.t@gmail.com>
2024-06-18 01:24:48 -0300

    Improve collision system

--- examples/collision/systems/AIMovementInputSystem.cpp
+++ examples/collision/systems/AIMovementInputSystem.cpp
@@ -18,29 +18,52 @@ namespace Engine::Examples::Collision {
     void AIMovementInputSystem::Update() {
         const int width = m_Window->GetWidth();
         const int height = m_Window->GetHeight();
-        const raylib::Rectangle rectangleLeft(0, 0, 1, height);
-        const raylib::Rectangle rectangleRight(width, 0, 1, height);
-        const raylib::Rectangle rectangleTop(0, 0, width, 1);
-        const raylib::Rectangle rectangleBottom(0, height, width, 1);
+        const raylib::Rectangle rectangleLeft(0, 0, .05f, height);
+        const raylib::Rectangle rectangleRight(width, 0, .05f, height);
+        const raylib::Rectangle rectangleTop(0, 0, width, .05f);
+        const raylib::Rectangle rectangleBottom(0, height, width, .05f);
         auto view = m_Registry->view<const Circle, const Position, MovementInput>(entt::exclude<PlayerRef>);
 
         view.each([&](const Circle &circle, const Position &position, MovementInput &movementInput) {
-            if (!CheckCollisionCircleRec(position.Value, circle.Radius, rectangleLeft) &&
-                !CheckCollisionCircleRec(position.Value, circle.Radius, rectangleRight) &&
-                !CheckCollisionCircleRec(position.Value, circle.Radius, rectangleTop) &&
-                !CheckCollisionCircleRec(position.Value, circle.Radius, rectangleBottom) &&
+            const bool collisionLeft = CheckCollisionCircleRec(position.Value, circle.Radius, rectangleLeft);
+            const bool collisionRight = CheckCollisionCircleRec(position.Value, circle.Radius, rectangleRight);
+            const bool collisionTop = CheckCollisionCircleRec(position.Value, circle.Radius, rectangleTop);
+            const bool collisionBottom = CheckCollisionCircleRec(position.Value, circle.Radius, rectangleBottom);
+
+            DrawLine(position.Value.x, position.Value.y, position.Value.x + movementInput.Direction.x * circle.Radius * 2,
+                     position.Value.y + movementInput.Direction.y * circle.Radius * 2, GREEN);
+
+            if (!collisionLeft &&
+                !collisionRight &&
+                !collisionTop &&
+                !collisionBottom &&
                 movementInput.Direction != raylib::Vector2::Zero()) {
                 return;
             }
 
-            movementInput.Direction =
-                    movementInput.Direction == raylib::Vector2::Zero()
-                    ? raylib::Vector2(GetRandomValue(-1, 1), GetRandomValue(-1, 1)).Normalize()
-                    : (-movementInput.Direction +
-                       movementInput.Direction.Rotate(GetRandomValue(-60, 60))).Normalize();
+            if (movementInput.Direction == raylib::Vector2::Zero()) {
+                movementInput.Direction = raylib::Vector2(GetRandomValue(-1, 1), GetRandomValue(-1, 1)).Normalize();
+            } else {
+                movementInput.Direction = raylib::Vector2::Zero();
 
+                if (collisionLeft) {
+                    movementInput.Direction.x += 1;
+                }
+
+                if (collisionRight) {
+                    movementInput.Direction.x -= 1;
+                }
+
+                if (collisionTop) {
+                    movementInput.Direction.y += 1;
+                }
+
+                if (collisionBottom) {
+                    movementInput.Direction.y -= 1;
+                }
+
+                movementInput.Direction = movementInput.Direction.Rotate(DEG2RAD * GetRandomValue(-60, 60)).Normalize();
+            }
         });
     }
-
-
 }
